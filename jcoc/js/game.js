5677cc9818bfd957b51da29ef7f2e3d28ccfa4e6
backoffbelief <backoffbelief@gmail.com>
2015-12-14 18:09:31 +0800

    coc

--- jcoc/js/game.js
+++ jcoc/js/game.js
@@ -1,22 +1,21 @@
 document.body.onload = main;
 
-var c;
-var cxt;
+var mapSize = 40;
+var gridSize = 8;
+
+var buildings;
+
+var userDatas;
+var gameUrl = "http://127.0.0.1:8000/login/";
+
 function main()
 {
-	init();
-	
-//	drawLines();
-	
-//	drawArcs();
-
-	drawPics();
 }
 
 function init()
 {
-	c = document.getElementById('myCanvas');
-	cxt = c.getContext('2d');
+//	c = document.getElementById('myCanvas');
+//	cxt = c.getContext('2d');
 }
 
 
@@ -73,14 +72,139 @@ function preLoadImage(url, callback)
 	}
 }
 
- function getData()
+ function httpReq(url, callback)
   {
   	$.ajax({
   		type:"get",
-  		url:"http://127.0.0.1/login/getUserInfo/op",
+  		url:url,
   		async:true,
-  		success:function(data){
-  			document.getElementById('content').innerHTML = JSON.stringify(data) ;
-  		}
-  	});
-  }
+  		success:callback});
+ }
+ 
+ function showUser()
+ {	
+ 	initAreaData();
+ 	httpReq(gameUrl+'getUserInfo/op0', function(data){
+ 		userDatas = data;
+ 		draw(userDatas);
+ 	});
+ }
+ 
+ function createNewBuilding()
+ {
+ 	initAreaData();
+ 	httpReq(gameUrl+'createNewBuild/4/1002/0_3', function(data){
+ 		if(data.status != 2)
+ 		{
+ 			alert(getErrcodeMsg(data.errcode));
+ 		}
+ 	});
+ }
+function initAreaData()
+{
+	$.getJSON('scripts/coc_bulid.json', function(data){
+		buildings = data;
+	});
+}
+
+function getSize(xmlId)
+{
+	for (var i = 0; i < buildings.length; i++) {
+		if(buildings[i].xmlId == xmlId)
+		{
+			return buildings[i].size;
+		}
+	}
+}
+
+function getErrcodeMsg(errcode)
+{
+	switch(errcode)
+	{
+		case 10005:return '建筑的位置重叠了，请重新摆放';
+	}
+	
+	return 'System error!';
+}
+
+function draw(data)
+{
+	var	c = document.getElementById('myCanvas');
+	var cxt = c.getContext('2d');
+	//requestAnimationFrame(draw);
+	cxt.strokeStyle ='blue';
+	for (var i = 0; i <= mapSize; i++) {
+		line0(cxt,i,0,i,mapSize);
+	}
+	for (var i = 0; i <= mapSize; i++) {
+		line0(cxt,0,i,mapSize,i);
+	}
+	
+	
+	if(data != 'undefined')
+	{
+		var objs = data['barriers'];
+		cxt.save();
+		cxt.fillStyle='green';
+		for (var i = 0; i < objs.length; i++) {
+			var size = getSize(objs[i].xmlId);
+			fill0(cxt,objs[i].posX,objs[i].posY,size);
+		}
+		cxt.restore();
+		cxt.save();
+		cxt.fillStyle='red';
+		objs = data['buildings'];
+		for (var i = 0; i < objs.length; i++) {
+			var size = getSize(objs[i].xmlId);
+			fill0(cxt,objs[i].posX,objs[i].posY,size);
+		}
+		cxt.restore();
+		if(data.user.id){
+			document.getElementById('id').innerHTML="id:"+data.user.id;
+		}
+		if(data.user.rank){
+			document.getElementById('rank').innerHTML="rank:"+data.user.rank;
+		}
+		if(data.user.platformId){
+			document.getElementById('platformId').innerHTML="platformId:"+data.user.platformId;
+		}
+		if(data.user.fame){
+			document.getElementById('fame').innerHTML="fame:"+data.user.fame;
+		}
+		if(data.user.dimaond){
+			document.getElementById('dimaond').innerHTML="dimaond:"+data.user.dimaond;
+		}
+	}
+	//setInterval(1000);
+}
+function line0(cxt,x,y,dstX,dstY){
+	cxt.moveTo((x+y)*gridSize, mapSize*gridSize- (x-y)*gridSize);
+	cxt.lineTo((dstX+dstY) * gridSize, mapSize*gridSize-(dstX - dstY)*gridSize);
+	cxt.stroke();
+}
+
+
+function fill0(cxt,x, y, size){
+	move0(cxt,x,y);
+	line1(cxt,x,y+size);
+	line1(cxt,x+size,y+size);
+	line1(cxt,x+size, y);
+	cxt.closePath();
+	cxt.fill();
+}
+
+function move0(cxt,x,y){
+	cxt.moveTo((x+y)*gridSize, mapSize*gridSize - (x-y)*gridSize);
+}
+
+function line1(cxt,x,y)
+{
+	cxt.lineTo((x+y)*gridSize, mapSize*gridSize - (x-y)*gridSize);
+}
+
+function deleteBarrier()
+{
+	httpReq(gameUrl+'deleteBarrier/4/',function(data){
+		
+	});
+}
